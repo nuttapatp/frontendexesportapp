3d969e33a61ff92303a91a87de1408d57c4ffaa7
Nuttapat Pothavichai <nuttapatp@outlook.co.th>
2023-09-20 17:13:38 +0700

    25 revised for history bar

--- src/components/Singleproduct.jsx
+++ src/components/Singleproduct.jsx
@@ -107,7 +107,7 @@ export default function SingleProduct() {
         <Categorybar />
       </div>
       <div className="history-bar">
-        <a href={`http://localhost:3000/singleproduct/${id}`}>
+        <a href={`https://frontendexesportapp.vercel.app/brands/${shoe.prod_name}`}>
           {shoe.brand_name}
         </a>
         <span>&gt;</span>
